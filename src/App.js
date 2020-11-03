14619b2cf48edc58158d93bbbd7cbb16277f1def
NehaYalala02 <69045005+NehaYalala02@users.noreply.github.com>
2020-11-03 18:16:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./style.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    };
+  }
+  componentDidMount() {
+    fetch("https://randomuser.me/api/")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        });
+      });
+  }
 
-export default function App() {
-	return <div>Hello world</div>;
+  onLoadMoreClick = () => {
+    this.componentDidMount();
+  };
+
+  render() {
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div>loading</div>;
+    } else {
+      return (
+        <div className="App">
+          <div className="card" key={items.results}>
+            <p>
+              Name: {items.results[0].name.title} {items.results[0].name.first}{" "}
+              {items.results[0].name.last}
+            </p>
+            <p> Gender: {items.results[0].gender} </p>
+            <p>Email: {items.results[0].email}</p>
+            <p>State: {items.results[0].location.state}</p>
+            <p>Country: {items.results[0].location.country}</p>
+            <p>Age: {items.results[0].dob.age}years</p>
+          </div>
+          <button className="loadMore" onClick={this.onLoadMoreClick}>
+            Load More Cards
+          </button>
+        </div>
+      );
+    }
+  }
 }
+export default App;
